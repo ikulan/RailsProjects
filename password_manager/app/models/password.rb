9124ec92c353f973f761cf27cb4021eff918c1fe
ikulan <ikulan12@gmail.com>
2024-05-10 12:20:52 -0700

    Complete password controllers and views

--- password_manager/app/models/password.rb
+++ password_manager/app/models/password.rb
@@ -1,7 +1,9 @@
 class Password < ApplicationRecord
-  has_many :user_passwords
+  has_many :user_passwords, dependent: :destroy
   has_many :users, through: :user_passwords
 
   encrypts :username, deterministic: true  # so this field is queryable
   encrypts :password
+
+  validates :username, presence: true
 end
