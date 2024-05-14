4bdedb5ffc3b5442d3e3d016a8b7eaa84cd9f521
ikulan <ikulan12@gmail.com>
2024-05-13 17:09:55 -0700

    Beautify devise forms and remove unused views

--- password_manager/app/models/user.rb
+++ password_manager/app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable,
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :user_passwords, dependent: :destroy
   has_many :passwords, through: :user_passwords
