ce9e62934ef8120c775eb8e5f30a892887e78d19
ikulan <ikulan12@gmail.com>
2024-05-17 16:56:58 -0700

    Add Links model and controller

---
+++ url_shortener/db/migrate/20240517233703_create_links.rb
@@ -0,0 +1,13 @@
+class CreateLinks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :title
+      t.string :description
+      t.string :preview_image
+      t.integer :views_count
+
+      t.timestamps
+    end
+  end
+end
