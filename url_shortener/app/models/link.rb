3ef1fcbcbe8c02a83091410ebc46275a0ae26e4a
ikulan <ikulan12@gmail.com>
2024-05-23 21:12:42 -0700

    Add randomize id

--- url_shortener/app/models/link.rb
+++ url_shortener/app/models/link.rb
@@ -1,5 +1,14 @@
 class Link < ApplicationRecord
+  before_create :randomize_id
+
   scope :recent_first, -> { order(created_at: :desc) }
   
   validates :url, presence: true
+
+  private
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(100_000_000)
+    end while Link.where(id: self.id).exists?
+  end
 end
