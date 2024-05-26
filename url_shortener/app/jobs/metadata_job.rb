ddc737cc08c02cb2ae72e662ade90a476aba3af8
ikulan <ikulan12@gmail.com>
2024-05-26 01:02:10 -0700

    Utilize turbo stream to update the view after the background job finished

--- url_shortener/app/jobs/metadata_job.rb
+++ url_shortener/app/jobs/metadata_job.rb
@@ -3,5 +3,6 @@ class MetadataJob < ApplicationJob
     link = Link.find(id)
     metadata = Metadata.retrieve_from(link.url)
     link.update(metadata.attributes)
+    link.broadcast_replace_to(link)
   end
 end
